fda02445b1fe49fce7d56a9eb67c2ac668511a67
Federico <federico.scarpati1989@gmail.com>
2021-01-13 13:34:24 +0100

    Inserimento delle bandierine

--- assets/js/main.js
+++ assets/js/main.js
@@ -14,6 +14,7 @@ let app = new Vue({
   data: {
 	  mySearch: "",
 	  movies:"",
+	  series:"",
 	  myApiKey:"7590ffcc8dd999cadeff1cfe7fcd8fc4"
   },
   methods: {
@@ -26,8 +27,26 @@ let app = new Vue({
 			let vote = Math.ceil(element.vote_average / 2);
 			// console.log(vote);
 			element.vote_average = vote;
-		  })
+			if (item.original_language == "en"){
+            item.original_language = "gb";
+          	} else if (item.original_language == "zh") {
+            item.original_language = "cn"
+          	} else if (item.original_language == "ko") {
+            item.original_language = "kr"
+         	 } else if(item.original_language == "vi"){
+            item.original_language = "vn";
+         	 }else if(item.original_language == "et"){
+            item.original_language = "ee";
+         	 }else if(item.original_language == "ja"){
+            item.original_language = "jp";
+         	 }else if(item.original_language == "da"){
+            item.original_language = "dk";
+         	 } else if(item.original_language == "hu"){
+            item.original_language = "ua";
+         	 }
+		
 
+		}
 	  });
 	},
   }
